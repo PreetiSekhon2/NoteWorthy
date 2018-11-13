06648069ffae5feacb9284025a4a14bfde7a68d8
bmoishe <moishebarnes@gmail.com>
2018-11-13 15:37:12 +0000

    update tests

--- spec/viewIndividualNoteSpec.js
+++ spec/viewIndividualNoteSpec.js
@@ -1,5 +1,7 @@
 note = new Note()
 
+note.create_new_note('this is a note')
+note.create_new_note('this is another note that is rarther longgg')
 
-test2 = new Test('Displays full details of selected Note - toequal', note.viewIndividualNote(4), 'toEqual', "Hello")
-test1 = new Test('Displays full details of selected Note - not equal', note.viewIndividualNote(4), 'notEqualTo', "H ello")
+test2 = new Test('Displays full details of selected Note - toequal', note.viewIndividualNote(0), 'toEqual', "this is a note")
+test1 = new Test('Displays full details of selected Note - not equal', note.viewIndividualNote(1), 'notEqualTo', "H ello")
