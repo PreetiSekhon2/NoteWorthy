7516323ea29d65e5d15836fd3faadac744b65425
Aimée Craig <aimeecraig1@gmail.com>
2018-11-14 10:16:34 +0000

    testing framework amended to use module structure

--- worthy/worthy.js
+++ worthy/worthy.js
@@ -1,96 +1,83 @@
-// function It(testDescription, expect) {
-//   this.testDescription = testDescription;
-//   this.expect = expect;
+(function(exports) {
+
+    var actualValue
+
+    function it(description) {
+      testDescription = description;
+
+      return {
+        expect: expect
+      }
+    }
+
+    function expect(actual) {
+      actualValue = actual;
+
+      return {
+        toEqual: toEqual
+      }
+    }
+
+    function toEqual(expectedValue) {
+      if (expectedValue == actualValue) {
+        document.write((testDescription + '<br><br>').fontcolor("green"))
+      } else {
+        document.write((testDescription + '<br>FAILED - Expected ' + expectedValue + ' but got ' + actualValue + '<br><br>').fontcolor("red"))
+      }
+    }
+
+    exports.it = it
+})(this)
+
+// ********** Old testing framework **********
+
+// var Test = function(description, testingFunction, testType, equalTo) {
+//   this.description = description;
+//   this.testingFunction = testingFunction
+//   this.equalTo = equalTo;
+//   this.output;
+//   this.comparitor(testType);
+//   this.outputResult();
+//
+//   // testType(testingFunction, equalTo)
 // }
 //
-// function Expect(test) {
-//   this.test = test;
+// Test.prototype.comparitor = function(type){
+//   switch(type) {
+//
+//     case 'toEqual':
+//       this.toEqual();
+//       break;
+//
+//     case 'notEqualTo':
+//       this.notEqualTo();
+//       break;
+//
+//     case 'include':
+//       //code
+//       break;
+//
+//     case 'notInclude':
+//       //code
+//       break;
+//
+//     default:
+//       console.log("No such comparator")
+//   }
 // }
-
-// (function(exports) {
-//   var testDescription;
 //
-//   function expect(testFunction) {
-//     testFunction = testFunction;
-//   }
-//
-//   function toEqual(value) {
-//     return this.test === value
-//   }
-//
-//   exports.toEqual = toEqual
-//   // exports.toEqual = toEqual
-//   // exports.expect.toEqual = toEqual
-//   // exports.toEqual = toEqual
-// })(this)
-
-// (function(exports) {
-//   function toEqual(value) {
-//     return expect === value
-//   }
-//
-//   exports.toEqual = toEqual
-// })(this)
-//
-// (function(exports) {
-//   function testFunction(test) {
-//     return test
-//   }
-//
-//   exports.testFunction = testFunction
-// })
-
-// expect(testFunction).toEqual = function(value) {
-//   testFunction === value;
+// Test.prototype.toEqual = function toEqual() {
+//   this.output = (this.testingFunction === this.equalTo);
 // }
-
-var Test = function(description, testingFunction, testType, equalTo) {
-  this.description = description;
-  this.testingFunction = testingFunction
-  this.equalTo = equalTo;
-  this.output;
-  this.comparitor(testType);
-  this.outputResult();
-
-  // testType(testingFunction, equalTo)
-}
-
-Test.prototype.comparitor = function(type){
-  switch(type) {
-
-    case 'toEqual':
-      this.toEqual();
-      break;
-
-    case 'notEqualTo':
-      this.notEqualTo();
-      break;
-
-    case 'include':
-      //code
-      break;
-
-    case 'notInclude':
-      //code
-      break;
-
-    default: 
-      console.log("No such comparator")
-  }
-}
-
-Test.prototype.toEqual = function toEqual() {
-  this.output = (this.testingFunction === this.equalTo);
-}
-
-Test.prototype.notEqualTo = function notEqualTo() {
-  this.output = this.testingFunction !== this.equalTo;
-};
-
-Test.prototype.outputResult = function(){
-  if (this.output){
-    document.write( (this.description + '<br><br>').fontcolor("green"))
-  } else {
-    document.write((this.description + '<br>FAILED - Expected ' + this.equalTo + ' but got ' + this.testingFunction + '<br><br>').fontcolor("red"))
-  }
-}
+//
+// Test.prototype.notEqualTo = function notEqualTo() {
+//   this.output = this.testingFunction !== this.equalTo;
+// };
+//
+// Test.prototype.outputResult = function(){
+//   if (this.output){
+//     document.write( (this.description + '<br><br>').fontcolor("green"))
+//   } else {
+//     document.write((this.description + '<br>FAILED - Expected ' + this.equalTo + ' but got ' + this.testingFunction + '<br><br>').fontcolor("red"))
+//   }
+// }
