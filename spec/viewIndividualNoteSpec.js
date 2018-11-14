8d907a8ef5678de030baba4b8d0d8f0a046360d5
Aimée Craig <aimeecraig1@gmail.com>
2018-11-14 10:29:26 +0000

    added toNotEqual matcher to Worthy framework

--- spec/viewIndividualNoteSpec.js
+++ spec/viewIndividualNoteSpec.js
@@ -4,7 +4,7 @@ notes3.create_new_note('this is a note')
 notes3.create_new_note('this is another note that is rather long')
 
 it('displays the full details of a seleted note').expect(notes3.viewIndividualNote(0)).toEqual('this is a note')
-it('displays the full details of a selected note').expect(notes3.viewIndividualNote(1)).toEqual('this is another note that is rather long')
+it('does not display the full details of a selected note').expect(notes3.viewIndividualNote(1)).toNotEqual('H ello')
 
 // test2 = new Test('Displays full details of selected Note - toequal', note.viewIndividualNote(0), 'toEqual', "this is a note")
 // test1 = new Test('Displays full details of selected Note - not equal', note.viewIndividualNote(1), 'notEqualTo', "H ello")
