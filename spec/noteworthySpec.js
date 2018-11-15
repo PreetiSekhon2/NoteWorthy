01a9ec7010de5746125124fc97795fd2753816b6
Aimée Craig <aimeecraig1@gmail.com>
2018-11-15 14:27:33 +0000

    README amended

--- spec/noteworthySpec.js
+++ spec/noteworthySpec.js
@@ -20,3 +20,6 @@ it('length should be 20').expect(notes2.list_abbreviated_notes()[0].length).toEq
 
 // var test_1 = new Test('returns an abbreviated list of notes should say "This is a test of an"', note_1.list_abbreviated_notes()[0],'toEqual', 'This is a test of an')
 // var test_2 = new Test('length sould be 20', note_1.list_abbreviated_notes()[0].length,'toEqual', 20)
+
+var mock = new DoubleObj('mockMethod', 4)
+it('returns a mocked method').expect(mock.mockMethod()).toEqual(4)
